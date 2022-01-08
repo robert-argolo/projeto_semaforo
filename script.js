d8931a058cef4c8efc6b73ed3d5c282133129fd3
robertbr000 <robert.s.argolo@gmail.com>
2022-01-08 18:51:06 -0300

    tempo diferente para cada cor

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ let img = document.getElementById("img");
 let obs = document.getElementById("obs");
 let containerBtns = document.getElementById("containerBtns");
 let indexDaCor = 0;
-intervaloId = null;
+let intervaloId = null;
+let tempoVerde = 6000;
+let tempoAmarelo = 2000;
+let tempoVermelho = 4000;
 
 //variáveis com funções
 let proximaCor = () => {
@@ -15,13 +18,37 @@ let mudarCorAutomatico = () => {
     let TodasAsCores = ["btnVerde","btnAmarelo","btnVermelho" ];
     let corDaVez = TodasAsCores[indexDaCor];
     acaoBtn[corDaVez]();
+    console.log(indexDaCor)
+    tempoMudarCor()
     proximaCor();
     
+    
 };
+function tempoMudarCor(){
+    if(indexDaCor == 0){
 
+        //tempo cor verde
+        pararAutomatico()
+        intervaloId = setInterval(mudarCorAutomatico, tempoVerde)
+        console.log("Cor verde")
+
+    }else if (indexDaCor == 1){
+
+        //tempo cor Amarela
+        console.log("Cor Amarela")
+        pararAutomatico()
+        intervaloId = setInterval(mudarCorAutomatico, tempoAmarelo )
+    }else if (indexDaCor == 2){
+
+        //tempo cor vermelha
+        console.log("Cor vemelha")
+        pararAutomatico()
+        intervaloId = setInterval(mudarCorAutomatico, tempoVermelho )
+    }
+}
 let pararAutomatico = () => {
     clearInterval(intervaloId)
-    // console.log(pararAutomatico());
+    
 }
 
 let descobrirBtn = (eventoClique) => {
@@ -34,7 +61,7 @@ let acaoBtn = {
   btnAmarelo: () => (img.src = "./imagens/amarelo.png"),
   btnVerde: () => (img.src = "./imagens/verde.png"),
   btnDesligar: () => (img.src = "./imagens/desligado.png"),
-  btnAutomatico: () => intervaloId = setInterval(mudarCorAutomatico, 1000),
+  btnAutomatico: () => intervaloId = setInterval(mudarCorAutomatico, 2000),
 };
 
 //eventos de clique
